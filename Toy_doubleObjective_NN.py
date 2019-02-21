cc68549356edfa6080c58fc869371386e71fe936
Unknown <marirsg2@gmail.com>
2019-02-21 14:26:54 -0700

    switch from binary to one hot input to output.
    target is to get the embedding to be binary encoding

--- Toy_doubleObjective_NN.py
+++ Toy_doubleObjective_NN.py
@@ -23,72 +23,104 @@ from random import shuffle
 #synthetic dataset
 # a long list of numbers which are then converted to one hot encoding or binary encoding.
 
-num_epochs = 5
-number_size = 10000
-input_vector_size = int(math.ceil(math.log(number_size,2)))
+num_epochs = 100
+number_size = 1000
+DISPLAY_METRIC_INTERVAL = number_size/10
+BATCH_SIZE = 1000
+
+embedding_vector_size = int(math.ceil(math.log(number_size, 2)))
 use_cuda = False
 # print(input_vector_size)
+# criterion = nn.BCEWithLogitsLoss()
 criterion = nn.BCELoss()
+# criterion = nn.CrossEntropyLoss()
+
+# data_set = []
+# for i in range(number_size):
+#     bin_value_str = bin(i)
+#     bin_value_array = [int(x) for x in bin_value_str[2:]]
+#     #add necessary preceeding zeros to fix the input size
+#     bin_value_array = [0.0] * (embedding_vector_size - len(bin_value_array)) + bin_value_array
+#     data_set.append(bin_value_array)
 
 data_set = []
 for i in range(number_size):
-    bin_value_str = bin(i)
-    bin_value_array = [int(x) for x in bin_value_str[2:]]
-    #add necessary preceeding zeros to fix the input size
-    bin_value_array = [0.0]*(input_vector_size-len(bin_value_array)) + bin_value_array
-    data_set.append(bin_value_array)
-
+    one_hot_array = [0.0] * number_size
+    one_hot_array[i] = 1.0
+    data_set.append(one_hot_array)
 
 shuffle(data_set)
 target_set = torch.tensor(data_set, dtype = torch.float)
+# NEED TO PUT INDICES in the target set if using cross entropy loss
+
 data_set = torch.tensor(data_set, dtype = torch.float)
 
 class AE_noSecond(nn.Module):
     def __init__(self):
         super(AE_noSecond,self).__init__()
-        self.fc1 = nn.Linear(input_vector_size,int(input_vector_size/2))
-        self.fc2 = nn.Linear(int(input_vector_size/2),int(input_vector_size/4))
+        self.fc1 = nn.Linear(number_size, int(number_size/2))
+        self.fc2 = nn.Linear(int(number_size/2), int(number_size/4))
+        # self.fc3 = nn.Linear(int(number_size/4), int(number_size/8))
+        self.fc4 = nn.Linear(int(number_size / 4), int(embedding_vector_size))
         #decode
-        self.fc3 = nn.Linear(int(input_vector_size/4),int(input_vector_size/2))
-        self.fc4 = nn.Linear(int(input_vector_size/2),input_vector_size)
+        self.fc5 = nn.Linear(int(embedding_vector_size), int(embedding_vector_size * 2))
+        self.fc6 = nn.Linear(int(embedding_vector_size * 2), int(number_size/2))
+        self.fc7 = nn.Linear(int(number_size/2), number_size)
 
     # -------------------------------
     def forward(self, input):
         curr_output = F.relu(self.fc1(input))
         curr_output = F.relu(self.fc2(curr_output))
-        curr_output = F.relu(self.fc3(curr_output))
-        curr_output = F.sigmoid(self.fc4(curr_output))
+        # curr_output = F.relu(self.fc3(curr_output))
+        curr_output = F.relu(self.fc4(curr_output))
+        curr_output = F.relu(self.fc5(curr_output))
+        curr_output = F.relu(self.fc6(curr_output))
+        #if criterion is BCE with logit loss, then no sigmoid. BUT to decode still use sigmoid
+        # curr_output = self.fc7(curr_output)
+        #else
+        curr_output = F.sigmoid(self.fc7(curr_output))
         return curr_output
     #-------------------------------
-    def get_encoding(self,input):
-        curr_output = F.relu(self.fc1(input))
-        curr_output = F.relu(self.fc2(curr_output))
-        return curr_output
-    # -------------------------------
-    def get_decoding(self,input):
-        curr_output = F.relu(self.fc3(input))
-        curr_output = F.sigmoid(self.fc4(curr_output))
-        return curr_output
+    # def get_encoding(self,input):
+    #     curr_output = F.relu(self.fc1(input))
+    #     curr_output = F.relu(self.fc2(curr_output))
+    # #     curr_output = F.relu(self.fc3(curr_output))
+    #     curr_output = F.relu(self.fc4(curr_output))
+    #     return curr_output
+    # # -------------------------------
+    # def get_decoding(self,input):
+    #     curr_output = F.relu(self.fc5(input))
+    #     curr_output = F.relu(self.fc6(curr_output))
+    #     curr_output = F.sigmoid(self.fc7(curr_output))
+    #     return curr_output
     # -------------------------------
 
 device = torch.device("cuda" if use_cuda else "cpu")
 
 ae_nn = AE_noSecond()
 ae_nn.to(device)
-optimizer = optim.SGD(ae_nn.parameters(), lr = 0.01, momentum = 0.5)
+optimizer = optim.SGD(ae_nn.parameters(), lr = 0.001)
 ae_nn.train()
 for i in range(num_epochs):
     print("At epoch =", i)
+    cumul_loss = 0.0
     for d in range(data_set.shape[0]):
         input,target = data_set[d:d+1,:],target_set[d:d+1,:]
         input,target = input.to(device),target.to(device)
-        optimizer.zero_grad()
         output = ae_nn(input)
         loss = criterion(output,target)
+        cumul_loss += loss.data
         loss.backward()
         optimizer.step()
-        if d%200 == 0:
+        optimizer.zero_grad()
+
+        # if d%BATCH_SIZE == 0:
+        #     optimizer.step()
+        #     optimizer.zero_grad()
+        if d%DISPLAY_METRIC_INTERVAL == 0:
             print("at d =",d)
+            print("avg loss %f",cumul_loss/200)
+            cumul_loss = 0.0
     #end inner for
 #end outer for
 
